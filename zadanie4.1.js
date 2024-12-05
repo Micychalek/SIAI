b13ad099c70b560ce8d11dabf85aa0f96c1830dc
Micy <87359225+Micychalek@users.noreply.github.com>
2024-12-05 14:43:23 +0000

    1234

--- zadanie4.1.js
+++ zadanie4.1.js
@@ -1,9 +1,9 @@
 
-        let gruby_greda = true;
-        console.log(gruby_greda)
-        if (gruby_greda){
-            console.log("Greda jest gruby") 
-        }
-        if (!gruby_greda){
-            console.log("Greda nie jest gruby") 
-        }
+let gruby_greda = true;
+console.log(gruby_greda)
+if (gruby_greda){
+    console.log("Greda jest gruby") 
+}
+if (!gruby_greda){
+    console.log("Greda nie jest gruby") 
+}
