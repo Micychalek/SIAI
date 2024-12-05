9a17ad71b964d6b29331b61eabe7eac4eea71c2a
Micy <87359225+Micychalek@users.noreply.github.com>
2024-12-05 14:44:02 +0000

    1234

--- milenakilometry.js
+++ milenakilometry.js
@@ -1,5 +1,5 @@
 
         let mile = 130;
         let kilometry = mile * 1.60934;
-        console.log(`Odległosc ${mile} mil odpowiada ${kilometry} kilometra`);
+        console.log("Odległosc ${mile} mil odpowiada ${kilometry} kilometra");
 
