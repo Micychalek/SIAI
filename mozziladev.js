92d18a0785fb9c2718ae412ef0001b9cd898700e
Micy <87359225+Micychalek@users.noreply.github.com>
2025-02-18 13:57:12 +0100

    1234

--- mozziladev.js
+++ mozziladev.js
@@ -1,11 +1,33 @@
-<button>Change color</button>
-const btn = document.querySelector("button");
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Guzik Kolor</title>
+    <style>
+      button {
+        margin: 10px;
+        font-size: 300%;
+        padding: 30px;
+      };
+    </style>
+  </head>
+  <body>
+    <button>zmien Kolor guzika</button>
+    <script>
+      const btn = document.querySelector('button');
 
-function random(number) {
-  return Math.floor(Math.random() * (number + 1));
-}
+      function random(number) {
+        return Math.floor(Math.random() * (number+1));
+      }
 
-btn.addEventListener("click", () => {
-  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-  document.body.style.backgroundColor = rndCol;
-});
+      function bgChange(e) {
+        const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+        e.target.style.backgroundColor = rndCol;
+        console.log(e);
+      }
+
+      btn.addEventListener('click', bgChange);
+    </script>
+  </body>
+</html>
