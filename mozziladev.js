f3876c5eb11b1d6b6d2c1c1fa73a2bc800f96cd3
Micy <87359225+Micychalek@users.noreply.github.com>
2025-02-18 14:09:45 +0100

    123333

--- mozziladev.js
+++ mozziladev.js
@@ -36,7 +36,7 @@
     const textBox = document.querySelector("#textBox");
     const output = document.querySelector("#output");
     textBox.addEventListener("keydown", (event) => {
-    output.textContent = `You pressed "${event.key}".`;
+    output.textContent = `nacisnales "${event.key}".`;
     });
 
     </script>
